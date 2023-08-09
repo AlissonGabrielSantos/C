8cb854197138ff41c3e4f361b31598df85ed8b2d
AlissonGabrielSantos <alisson.gabriel@ufpr.br>
2023-08-09 17:08:33 -0300

    Melhorando ex4

--- exercicio4.c
+++ exercicio4.c
@@ -1,23 +1,22 @@
 #include <stdio.h>
+#define MAX_NUM 16
 
-int main()
-{
-    unsigned char t;
-    int i;
-    for (i = 0; i <= 0xF; i++)
-    {
-        printf("%2X", i);
+int main() {
+    int asc, col, lin;
+    printf("  ");
+    for (int col = 0; col < MAX_NUM; col++) {
+        printf("%2x", col);
     }
     printf("\n");
-    for (i = 0, t = 0; i <= 255; i++, t++)
-    {
-        t > 32 ? printf("%2c", t) : printf("%2c", 32);
 
-        if (t % 16 == 0)
-        {
-            printf("\n");
+    for (int lin = 0; lin < MAX_NUM; lin++) {
+        printf("%2X ", lin);
+        for (int col = 0; col < MAX_NUM; col++) {
+            int asc = lin * MAX_NUM + col;
+            printf("%c ", (asc < 32) ? 32 : asc);
         }
+        printf("\n");
     }
-    printf("\n");
+
     return 0;
 }
