6d6861bbfacd352160860156b4a2cd30ff55a06a
AlissonGabrielSantos <alisson.gab.santos@gmail.com>
2023-08-30 16:36:30 -0300

    Lista 2 Ex3

---
+++ lista_2/ex3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/*  Faça um programa que leia uma matriz de
+    inteiros de 3x3 e apresente os elementos
+    da diagonal secundária.
+*/
+int main(void){
+    int matriz[3][3];
+    for (int i = 0, j = 0; i < 3; i++){
+        for (j = 0; j < 3; j++){
+            scanf("%d", &matriz[i][j]);
+        }
+    }
+    printf("%d\n%d\n%d\n", matriz[0][2], matriz[1][1], matriz[2][0]);
+}
